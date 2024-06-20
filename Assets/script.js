0e6dce2e9ad3234fbfa46af951f389bb711189e0
lammyy5 <reliford2010@gmail.com>
2024-06-20 07:34:41 -0700

    added a function to get the weather data from the api call

--- Assets/script.js
+++ Assets/script.js
@@ -4,21 +4,14 @@ const currentWeatherEl = document.querySelector('#current-weather-info');
 const forecastEl = document.querySelector('#forecast-container');
 const cityNameEl = document.querySelector('#city-input');
 const cityFormEl = document.querySelector('#city-form')
-// let apiCity = cityName + '&limit=2&appid=' + apiKey;
-let geoUrl =
-	'http://api.openweathermap.org/geo/1.0/direct?q=' +
-	cityNameEl +
-	'&limit=2&appid=cc20c2bbfb885577a8885d75622aaae4';
-let apiKey = 'cc20c2bbfb885577a8885d75622aaae4'
-// let apiUrl =
-// 	'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+
 let formSubmitHandler = function (event) {
   event.preventDefault();
 
   let cityName = cityNameEl.value.trim();
-console.log(typeof cityName)
+  console.log(typeof cityName)
   if (cityName) {
-    // apiCall(cityName);
+    apiCall(cityName);
 
     cityNameEl.textContent = '';
 
@@ -26,16 +19,37 @@ console.log(typeof cityName)
     alert('Please enter a GitHub username');
   }
 };
-function apiCall(){
-
-// let 
-// console.log(cityName)
-    fetch(geoUrl)
-    .then(function (response){
-        return response.json();
+function apiCall(cityName) {
+  let apiKey = 'cc20c2bbfb885577a8885d75622aaae4'
+  let geoUrl =
+    'http://api.openweathermap.org/geo/1.0/direct?q=' +
+    cityName +
+    '&limit=2&appid=' + apiKey;
+  // let apiUrl =
+// 	'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+  // console.log(cityName)
+  fetch(geoUrl)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function (data){
-        console.log(data)
-    })}
+    .then(function (data) {
+      console.log(data[0].lat)
+      getWeather(data,apiKey)
+    })
+}
+function getWeather(data,apiKey){
+let lat = data[0].lat
+let lon = data[0].lon
+let apiUrl =
+	'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
+  fetch(apiUrl)
+  .then(function (response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data)
+    return data
+  })
+}
 
 cityFormEl.addEventListener('submit', formSubmitHandler)
